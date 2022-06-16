16962752adcde3e534b619b0f47f19f03a6a601d
Nông Ngọc Hoài <ngochoaitn@gmail.com>
2022-06-16 10:22:29 +0700

    Update api python

--- python/gpm_login_api.py
+++ python/gpm_login_api.py
@@ -5,8 +5,9 @@ import time
 from selenium.webdriver.chrome import service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
 
-from UndetectChromeDriver import UndetectChromeDriver
+# from UndetectChromeDriver import UndetectChromeDriver
 #               ------------------------------------------------------
 #              * Code theo hướng dẫn tại file: 
 #              *              <THƯ_MỤC_GPM>/docs/api.md
@@ -18,7 +19,7 @@ from UndetectChromeDriver import UndetectChromeDriver
 #             // Bước 1:
 #             // Gọi commandline vào GPMLogin
 #             // Bạn có thể mở Profile có sẵn hoặc tạo Profile mới (cấu hình random) qua việc theo tác commandline với GPMLogin.exe
-gpmPath = "C:\\Users\\frien\\Desktop\\Test public tools\\GPMLogin_full_132"; # Đường dẫn tới thư mục tool GPMLogin
+gpmPath = "C:\\Users\\LEGION\\OneDrive\\Desktop\\New folder\\GPMLogin_full_124"; # Đường dẫn tới thư mục tool GPMLogin
 remotePort = 11269 # random port
 profileId = "isfboozwefoxiwg5srfdhtbohjgsd1tm8xw8mxvsg3jo4g0yz6"; # Copy ID trên App GPMLogin
 
@@ -26,7 +27,7 @@ profileId = "isfboozwefoxiwg5srfdhtbohjgsd1tm8xw8mxvsg3jo4g0yz6"; # Copy ID trê
 args = [gpmPath + '\\GPMLogin.exe', '--mode=open', f'--profile_id={profileId}', f'--remote_port={remotePort}', '--turn-off-whats-new']
 
 # hoặc tạo profile mới
-args = [gpmPath + '\\GPMLogin.exe', '--mode=new', f'--profile_name=ngochoaitn-gpm-test', f'--remote_port={remotePort}', '--turn-off-whats-new']
+args = [gpmPath + '\\GPMLogin.exe', '--mode=new', f'--profile_name=ngochoaitn-gpm-test', f'--remote_port={remotePort}', '--turn-off-whats-new']#, '--proxy-server=1.1.1.1:222']
 
 subprocess.call(args, cwd=gpmPath) # phải có cwd nha các bác <3
 time.sleep(3)
@@ -34,26 +35,14 @@ time.sleep(3)
 # Khởi tạo selenium AntiDetect kết nối vào port đã khởi tạo
 options = Options()
 options.debugger_address = f'127.0.0.1:{remotePort}'
-myService  = service.Service("C:\\Users\\frien\\Desktop\\Test public tools\\GPMLogin_full_132\\gpmdriver.exe")
-driver = UndetectChromeDriver(service = myService, options=options)
 
-driver.GetByGpm('https://nowsecure.nl/')
+driver = webdriver.Chrome(gpmPath + "\\gpmdriver.exe", options=options)
 
-"""
-driver.GetByGpm('https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
-time.sleep(3)
+# test undetect
+# myService  = service.Service(gpmPath + "\\gpmdriver.exe")
+# driver = UndetectChromeDriver(service = myService, options=options)
+# driver.GetByGpm('https://nowsecure.nl/')
 
-driver.find_element_by_name("identifier").send_keys('ngochoaitn3')
-time.sleep(1)
-
-driver.find_element_by_name("identifier").send_keys(Keys.RETURN)
-time.sleep(3)
-
-driver.find_element_by_name("password").send_keys('test1')
-time.sleep(1)
-
-driver.find_element_by_name("password").send_keys(Keys.RETURN)
-time.sleep(0.5)
-"""
+driver.get('https://google.com/')
 
 input()
